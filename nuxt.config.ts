807459c2b1921970ee9816368873262c5a49ca52
AbhishekAligh <abhishekaligh@gmail.com>
2024-05-15 13:19:11 +0530

    Add nuxtUI, nuxt/image and tailwind CSS.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,15 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "Construction Management System.",
+      title: "Construction Management System",
     },
   },
+
   devtools: { enabled: true },
-})
+  modules: ["@nuxt/ui", "@vueuse/motion/nuxt", "@nuxt/image"],
+
+  colorMode: {
+    preference: "system", // default value of $colorMode.preference
+    fallback: "light", //
+  },
+});
